27e4e6446025d5780ec289ef9d85672c7381c733
Reinhard Immanuel Abraham <reinhard_immanuel_abraham@yahoo.com>
2020-02-07 18:14:41 +0700

    Push BCABOS

---
+++ app/src/main/java/com/example/bcabos/BOSBCA.kt
@@ -0,0 +1,12 @@
+ package com.example.bcabos
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class BOSBCA : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
