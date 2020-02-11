9dcd99397f01c610cb0fed081085a6ea67fb21e7
Reinhard Immanuel Abraham <reinhard_immanuel_abraham@yahoo.com>
2020-02-11 09:21:35 +0700

    Merubah Flow Tampilan
    Home --> Feature

---
+++ app/src/main/java/com/example/bca_bos/KeyboardBOS.java
@@ -0,0 +1,197 @@
+package com.example.bca_bos;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class KeyboardBOS extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private static final String TAG = KeyboardBOS.class.getSimpleName();
+    private boolean capslock=false;
+    private boolean punctuation=false;
+    private boolean isInputConnectionExternalBOSKeyboard;
+
+    private KeyboardView kv;
+    private Keyboard keyboardQwerty, keyboardPunctuation;
+    private View root;
+
+    //HOME
+    private LinearLayout llHome;
+    private ImageButton btnBOS;
+
+    //FEATURE
+    private LinearLayout llFeature;
+    private Button btnCekOngkir, btnTemplateChat, btnRekapPesanan;
+
+    @Override
+    public View onCreateInputView() {
+
+        isInputConnectionExternalBOSKeyboard = true;
+        root = getLayoutInflater().inflate(R.layout.keyboard, null);
+        initiateKeyboardView();
+
+        try{
+            initiateMenu();
+            initiateFeature();
+        } catch (Exception ex){
+            Log.i(TAG, ex.toString());
+        }
+
+        return root;
+    }
+
+    private void initiateKeyboardView() {
+        keyboardQwerty = new Keyboard(this, R.xml.qwerty);
+        keyboardPunctuation = new Keyboard(this, R.xml.punctuation);
+        kv = root.findViewById(R.id.keyboard);
+        kv.setKeyboard(keyboardQwerty);
+        kv.setOnKeyboardActionListener(this);
+        kv.setPreviewEnabled(false);
+    }
+
+    private void initiateMenu() {
+        llHome = root.findViewById(R.id.homeLinearLayout);
+
+        btnBOS = root.findViewById(R.id.bosButton);
+        btnBOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFeatureMenu();
+            }
+        });
+
+    }
+
+    private void initiateFeature() {
+        llFeature = root.findViewById(R.id.featureLinearLayout);
+    }
+
+    private void showFeatureMenu() {
+        llFeature.setVisibility(View.VISIBLE);
+        llHome.setVisibility(View.GONE);
+        changeLayoutStatus(false);
+    }
+
+    private void changeLayoutStatus(Boolean homeStatus){
+        this.isInputConnectionExternalBOSKeyboard = homeStatus;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+        InputConnection ic = getCurrentInputConnection();
+        playClick(i);
+        switch (i){
+            case KeyboardKey.CAPSLOCK:
+                capslock = !capslock;
+                setCapslock(capslock);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1,0);
+            break;
+            case Keyboard.KEYCODE_SHIFT:
+                capslock = !capslock;
+                setCapslock(capslock);
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            case KeyboardKey.CHANGE_KEYBOARD:
+                changeKeyboardLayout();
+                break;
+            case KeyboardKey.PUNCTUATION:
+                punctuation = !punctuation;
+                changeToPunctuationKeyboardLayout(punctuation);
+                break;
+                default:
+                    char code = (char) i;
+                    if (Character.isLetter(code) && capslock)
+                        code = Character.toUpperCase(code);
+                    ic.commitText(String.valueOf(code),1);
+        }
+
+    }
+
+    private void playClick(int i) {
+
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch (i){
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+                default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+
+    private void setCapslock(boolean isCapslock){
+        kv.setShifted(isCapslock);
+        kv.invalidateAllKeys();
+    }
+
+    private void changeToPunctuationKeyboardLayout(boolean isPunctuation){
+        if(!isPunctuation) {
+            kv.setKeyboard(keyboardPunctuation);
+        } else {
+            kv.setKeyboard(keyboardQwerty);
+        }
+        kv.invalidateAllKeys();
+    }
+
+    private void changeKeyboardLayout(){
+        InputMethodManager imeManager = (InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+        imeManager.showInputMethodPicker();
+    }
+
+}
---
+++ app/src/main/java/com/example/bca_bos/KeyboardKey.java
@@ -0,0 +1,10 @@
+package com.example.bca_bos;
+
+public class KeyboardKey {
+    public static final int CAPSLOCK = -2;
+    public static final int CHANGE_KEYBOARD = 3;
+    public static final int KEYCODE_DONE = -4;
+    public static final int KEYCODE_DELETE = -5;
+    public static final int EMOJI = 7;
+    public static final int PUNCTUATION = -6;
+}
